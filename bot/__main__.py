8a5b8136248749bc3ae35b0a79632bf3eb4103f7
misershaad <91933177+misershaad@users.noreply.github.com>
2022-05-05 06:43:31 +0600

    speedtest added

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,7 +8,7 @@ from sys import executable
 from telegram import InlineKeyboardMarkup
 from telegram.ext import CommandHandler
 
-from bot import bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, alive, OWNER_ID, LOGGER, Interval, rss_session, INCOMPLETE_TASK_NOTIFIER, DB_URI
+from bot import bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, alive, LOGGER, Interval, rss_session, INCOMPLETE_TASK_NOTIFIER, DB_URI
 from .helper.ext_utils.fs_utils import start_cleanup, clean_all, exit_clean_up
 from .helper.ext_utils.telegraph_helper import telegraph
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
@@ -18,7 +18,7 @@ from .helper.telegram_helper.message_utils import sendMessage, sendMarkup, editM
 from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
 
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, count, leech_settings, search, rss
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, count, leech_settings, search, rss, speedtest
 
 
 
@@ -249,20 +249,17 @@ def main():
         notifier_dict = DbManger().get_incomplete_tasks()
         if notifier_dict:
             for cid, data in notifier_dict.items():
-                if bot.get_chat(cid).type in ['private', 'group']:
-                    continue
                 if ospath.isfile(".restartmsg"):
                     with open(".restartmsg") as f:
                         chat_id, msg_id = map(int, f)
                     msg = 'Restarted successfully!'
                 else:
                     msg = 'Bot Restarted!'
-                for tag, mids in data.items():
+                for tag, links in data.items():
                      msg += f"\n\n{tag}: "
-                     for index, mid in enumerate(mids, start=1):
-                         link = f"https://t.me/c/{str(cid)[4:]}/{mid}"
+                     for index, link in enumerate(links, start=1):
                          msg += f" <a href='{link}'>{index}</a> |"
-                         if len(msg.encode('utf-8')) > 4000:
+                         if len(msg.encode()) > 4000:
                              if 'Restarted successfully!' in msg and cid == chat_id:
                                  bot.editMessageText(msg, chat_id, msg_id, parse_mode='HTMl')
                                  osremove(".restartmsg")
@@ -274,7 +271,6 @@ def main():
                      osremove(".restartmsg")
                 else:
                     bot.sendMessage(cid, msg, 'HTML')
-        DbManger().trunc_table('notifier')
 
     if ospath.isfile(".restartmsg"):
         with open(".restartmsg") as f:
